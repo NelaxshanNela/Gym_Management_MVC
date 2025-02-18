626a8220f7a1b9e1396a9b65f783d7c31015f36d
Nelaxshan UCA <nelaxshan@unicornconnectedapps.com>
2025-02-18 17:38:08 +0530

    first commit

---
+++ GymManagementSystem_MVC/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using GymManagementSystem_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementSystem_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Dumbbells", Price = 5000 },
+            new Product { Id = 2, Name = "Treadmill", Price = 30000 }
+        };
+            return View(products);
+        }
+    }
+}
---
+++ GymManagementSystem_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymManagementSystem_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
